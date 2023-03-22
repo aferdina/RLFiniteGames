2670d9f5ef8b7ef15bfdf219e57d00ab7ca31430
aferdina <aferdina@mail.uni-mannheim.de>
2023-03-22 13:08:01 +0100

    clean up discrete agent

--- rlfinitegames/policies/discrete_agents.py
+++ rlfinitegames/policies/discrete_agents.py
@@ -1,26 +1,12 @@
-import gym
-
+""" create agents for finite games"""
 from typing import Optional, Union
-from gym import spaces
+from gym import Env, spaces
 import numpy as np
-from abc import ABC, abstractmethod
+from rlfinitegames.environments.grid_world import GridWorld
+from rlfinitegames.policies.base_agent import BaseAgent
 
-
-class BaseAgent(ABC):
-    def __init__(self, seed: Optional[int] = None, masking: bool = True):
-        self.rng = np.random.default_rng(seed=seed)
-        self.masking = masking 
-        
-
-    @abstractmethod
-    def get_action(self, state: np.array) -> int:
-        pass
-
-    @abstractmethod
-    def update_masking(self, state: np.array) -> int:
-        pass
-
-class FiniteAgent():
+# pylint: disable=too-few-public-methods
+class FiniteAgent(BaseAgent):
     """ The Agent class enables to play different policies for a given evironment
 
     :param env: The environment to learn from (if registered in Gym, can be str)
@@ -29,39 +15,55 @@ class FiniteAgent():
     :param policy_type: the type of the initialization policy
     """
 
-    def __init__(self, env: Union[gym.Env, str] = GridWorld(5), masking: bool = True, seed: Optional[int] = None, policy_type: str = 'uniform') -> None:
+    def __init__(self, env: Union[Env, str] = GridWorld(5),
+                 masking: bool = True, seed: Optional[int] = None,
+                 policy_type: str = 'uniform') -> None:
+        super().__init__(seed, masking)
         self.env = env
-        self.masking = masking
-        self.rng = np.random.default_rng(seed=seed)
         # size of action space
         if isinstance(self.env.action_space, spaces.Discrete):
             num_acts = self.env.action_space.n
             self.all_actions = np.arange(num_acts)
+        else:
+            raise ValueError(
+                "The action space is not discrete, and yet not supported")
         if isinstance(self.env.observation_space, spaces.MultiDiscrete):
             self.obs_shape = self.env.observation_space.nvec
             self.state_type = 'MultiDiscrete'
-        if isinstance(self.env.observation_space, spaces.Discrete):
+        elif isinstance(self.env.observation_space, spaces.Discrete):
             self.obs_shape = self.env.observation_space.n
             self.state_type = 'Discrete'
+        else:
+            raise ValueError(
+                "The observation space is not discrete or Multidiscrete, and yet not supported")
+
         policy_shape = np.append(self.obs_shape, num_acts)
 
-        # Init policy # TODO: Als Funktion auslagern?
+        self.policy = None
+        self._create_init_policy(policy_type, policy_shape, num_acts)
+
+    def _create_init_policy(self, policy_type: str,
+                            policy_shape: np.ndarray,
+                            num_acts: int) -> None:
         if policy_type == 'uniform':
             self.policy = np.ones(policy_shape)/num_acts
-        if policy_type == 'greedy' and self.state_type == 'MultiDiscrete':
+        elif policy_type == 'greedy' and self.state_type == 'MultiDiscrete':
             self.policy = np.zeros(policy_shape)
             for i in range(self.obs_shape[0]):
                 for j in range(self.obs_shape[1]):
                     k = np.random.randint(num_acts)
                     self.policy[i, j, k] = 1
-        if policy_type == 'greedy' and self.state_type == 'Discrete':
+        elif policy_type == 'greedy' and self.state_type == 'Discrete':
             self.policy = np.zeros(policy_shape)
             for i in range(self.obs_shape[0]):
                 for j in range(self.obs_shape[1]):
                     k = np.random.randint(num_acts)
                     self.policy[i, j, k] = 1
+        else:
+            raise ValueError(
+                "The policy type is not valid, and yet not supported")
 
-    def get_action(self, state: Union[np.ndarray,int]) -> int:
+    def get_action(self, state: Union[np.ndarray, int]) -> int:
         """samples an action of the environments action space for a given state
 
         Args:
@@ -70,8 +72,6 @@ class FiniteAgent():
         Returns:
             action :single action, randomly generated according to policy
         """
-        # TODO: assert for state shape
-        #assert state.shape == self.obs_shape.shape, "shape of state is {state.shape}, should be {self.obs_shape.shape}"
 
         # Update Probabilities
         self._update_action_mask_prob(state)
@@ -81,7 +81,7 @@ class FiniteAgent():
         action = self.rng.choice(self.all_actions, p=self.policy[state])
         return action
 
-    def _update_action_mask_prob(self, state: Union[np.ndarray,int]) -> None:
+    def _update_action_mask_prob(self, state: Union[np.ndarray, int]) -> None:
         """Updates the probabilities of all actions for a given state
 
         Args:
@@ -98,4 +98,3 @@ class FiniteAgent():
             state_prob[list(not_pos_actions)] = 0.0
             state_prob = state_prob/sum(state_prob)
             self.policy[state] = state_prob
-
