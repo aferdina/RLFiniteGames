823194b665b9d9a4f712d06c1c652041279e98c0
aferdina <aferdina@mail.uni-mannheim.de>
2023-04-09 10:19:42 +0200

    add setup logger file

---
+++ rlfinitegames/logging_module/setup_logger.py
@@ -0,0 +1,49 @@
+"""setup logging for a module"""
+import logging
+from typing import Union
+import colorlog
+
+LOG_COLORS = {
+    'DEBUG': 'cyan',
+    'INFO': 'white',
+    'WARNING': 'yellow',
+    'ERROR': 'red',
+    'CRITICAL': 'red,bg_white',
+}
+
+
+def setup_logger(logger_name: str = __name__,
+                 logger_level: int = logging.DEBUG,
+                 log_file: Union[str, None] = None,
+                 file_handler_level: int = logging.DEBUG,
+                 console_output: bool = False,
+                 stream_handler_level: int = logging.DEBUG) -> logging.Logger:
+    # Create a logger object
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logger_level)
+
+    # Create a formatter for the log messages
+    file_formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s')
+    console_formatter = colorlog.ColoredFormatter(
+        '%(log_color)s%(asctime)s %(name)s %(levelname)s:%(message)s',
+        datefmt=None,
+        reset=True,
+        log_colors=LOG_COLORS,
+        secondary_log_colors={},
+        style='%')
+    # Create a file handler if a file path is provided
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(file_handler_level)
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+
+    # Create a stream handler if console output is requested
+    if console_output:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(stream_handler_level)
+        stream_handler.setFormatter(console_formatter)
+        logger.addHandler(stream_handler)
+
+    return logger
