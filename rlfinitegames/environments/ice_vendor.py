7faf75b016767e6b9f388badba0bb2a4133beede
aferdina <aferdina@mail.uni-mannheim.de>
2023-04-20 13:43:22 +0200

    clean up ice vendor

--- rlfinitegames/environments/ice_vendor.py
+++ rlfinitegames/environments/ice_vendor.py
@@ -1,13 +1,15 @@
 """ Implementation of Ice Vendor example from lecture"""
 from dataclasses import dataclass
-from typing import Dict, Optional
+from typing import Dict, Optional, List
 from enum import Enum
 from gym import spaces, Env
 # pylint: disable=C0301
 from rlfinitegames.environments.utils.demandstructure import PoissonRandomVariable, BinomialRandomVariable, NegativeBinomialRandomVariable
 from rlfinitegames.environments.utils.helpfunctions import rgetattr
 
+# we start with an empty storage
 START_STATE = 0
+
 
 class DemandStructure(Enum):
     """ Different type of Demand structures """
@@ -30,9 +32,15 @@ class GameConfig:
 
 
 class IceVendor(Env):
-    """ Ice Vendor Environment """
+    """ Ice Vendor Environment
+    """
 
-    def __init__(self, game_config: GameConfig):
+    def __init__(self, game_config: GameConfig) -> None:
+        """ initialize ice vendor environment
+
+        Args:
+            game_config (GameConfig): including game configuration like selling price, demand structure
+        """
 
         # initialize the game config
         self.game_config = game_config
@@ -54,9 +62,15 @@ class IceVendor(Env):
         self.state = None
         self.reset()
 
-
     def step(self, action: int) -> list[int, float, bool, dict]:
-        """ run one step in the environment """
+        """ run a step in the ice vendor game
+
+        Args:
+            action (int): amount of ice cream to buy
+
+        Returns:
+            list[int, float, bool, dict]: next state, reward, information if finish, and info
+        """
 
         # getting demand
         demand = self.demand_structure.sample()
@@ -81,7 +95,14 @@ class IceVendor(Env):
         return next_state, reward, done, info
 
     def calculate_storage_cost(self, state: int) -> float:
-        """ calculate storage cost given state"""
+        """ calculate the storage costs for a given state
+
+        Args:
+            state (int): amount of ice cream in the storage
+
+        Returns:
+            float: storage costs for the given state
+        """
         return self.game_config.storage_cost * state
 
     def calculate_production_cost(self, action: int) -> float:
@@ -89,7 +110,14 @@ class IceVendor(Env):
         return self.game_config.production_cost * action
 
     def calculate_selling_price(self, sold_products: int) -> float:
-        """calculates the selling price given the sold products"""
+        """ calculates the selling price given the sold products
+
+        Args:
+            sold_products (int): amount of sold products
+
+        Returns:
+            float: cash making by sold products
+        """
         return self.game_config.selling_price * sold_products
 
     def reset(
@@ -98,14 +126,29 @@ class IceVendor(Env):
         seed: Optional[int] = None,
         options: Optional[dict] = None,
     ) -> None:
+        """ reset the game
+
+        Args:
+            seed (Optional[int], optional): Random seed for the game. Defaults to None.
+            options (Optional[dict], optional): Some options i dont even know. Defaults to None.
+        """
+        # TODO: Can someone explain Andre the options attribute from the base class
         super().reset(seed=seed)
         self.max_steps = 100
         self.timestep = 0
         self.state = START_STATE
 
-    def calculate_probability(self, state: int, action: int) -> float:
-        """calculate the probability to get in the next by taking action in specific state"""
-        prob_next_state = [0 for _ in range(
+    def calculate_probability(self, state: int, action: int) -> List[float]:
+        """calculate the probability to get in the next by taking action in specific state
+
+        Args:
+            state (int): amount of ice cream in the storage
+            action (int): amount of ice cream bought
+
+        Returns:
+            List[float]: Probability vector for all of the next states
+        """
+        prob_next_state = [0.0 for _ in range(
             self.game_config.max_inventory + 1)]
 
         next_state_without_demand = min(
@@ -118,22 +161,41 @@ class IceVendor(Env):
         return prob_next_state
 
     def get_rewards(self, state: int, action: int) -> list[float]:
-        """get the rewards for a given state and action"""
+        """get the rewards for a given state and action
+
+        Args:
+            state (int): amount of ice cream in the storage
+            action (int): amount of ice cream bought
+
+        Returns:
+            list[float]: list of rewards for all possible next states
+        """
         rewards = []
         for next_state in range(self.game_config.max_inventory + 1):
-            #what is the number of sold products
+            # what is the number of sold products
             # calculating reward
             reward = self.calculate_selling_price(
                 state + action - next_state) - self.calculate_storage_cost(next_state) - self.calculate_production_cost(action)
             rewards.append(reward)
         return rewards
 
+    def render(self, _mode: str = "human") -> None:
+        """ rendering the game
 
-    def render(self, _mode: str = "human"):
+        Args:
+            _mode (str, optional): mode to render the game. Defaults to "human".
+        """
         print(self.info)
 
     def get_valid_actions(self, state: int) -> list[int]:
-        """get the valid actions for a given state"""
+        """ get the valid actions for a given state
+
+        Args:
+            state (int): amount of ice cream in the storage
+
+        Returns:
+            list[int]: list with all valid actions for the given state
+        """
         valid_actions = list(range(
             self.game_config.max_inventory + 1 - state))
         return valid_actions
