260c36b784b83923230cadd87803efbb3ff7c13f
aferdina <aferdina@mail.uni-mannheim.de>
2023-04-27 18:35:21 +0200

    wip

--- rlfinitegames/experiments/run_double_qlearning/run_doubel_q.py
+++ rlfinitegames/experiments/run_double_qlearning/run_doubel_q.py
@@ -2,31 +2,30 @@ from rlfinitegames.environments.grid_world import GridWorld
 from rlfinitegames.algorithms.double_state_action_learning import DoubleParameter
 from rlfinitegames.environments.constructed_max_bias import ConstructedMaxBias
 from rlfinitegames.algorithms.policy_iteration import FiniteAgent
-from rlfinitegames.algorithms.helperclasses import RunTimeMethod, UpdateMethod, TruncatedBounds, PolicyMethod
+from rlfinitegames.algorithms.helperclasses import RunTimeMethod, UpdateMethod, TruncatedBounds
 from rlfinitegames.algorithms.double_state_action_learning import DoubleStateActionLearning, DoubleParameter
 
 
 def main():
     TOTALSTEPS = 10
-    # SIZE = 5
-    # parameter = DoubleParameter(
-    #     epsilon=0.01, runtimemethod=RunTimeMethod.EPISODES.value, episodes=10000, updatemethod=UpdateMethod.TRUNCUATED.value, trunc_bounds=TruncatedBounds(lower_bound=10.0,upper_bound=10.0))
-    # env = GridWorld(size=SIZE)
-    # agent = FiniteAgent(env)
-    # algo = DoubleStateActionLearning(
-    #     environment=env, policy=agent, algo_params=parameter, policy_method=PolicyMethod.EPSILONGREEDY)
-    # algo.run_double_state_action_learning()
-    # env.reset()
-    ### Constructed Max Bias ###
-
-    env = ConstructedMaxBias(number_arms=5)
-    agent = FiniteAgent(env=env, policy_type="uniform")
-    parameter = DoubleParameter(
-        epsilon=0.01, runtimemethod=RunTimeMethod.EPISODES.value, episodes=10000, updatemethod=UpdateMethod.TRUNCUATED.value, trunc_bounds=TruncatedBounds(lower_bound=10.0, upper_bound=10.0), method=PolicyMethod.BEHAVIOUR.value)
+    SIZE = 5
+    parameter = DoubleParameter()
+    env = GridWorld(size=SIZE)
+    agent = FiniteAgent(env)
     algo = DoubleStateActionLearning(
         environment=env, policy=agent, algo_params=parameter)
     algo.run_double_state_action_learning()
     env.reset()
+    ### Constructed Max Bias ###
+
+    # env = ConstructedMaxBias(number_arms=5)
+    # agent = FiniteAgent(env=env, policy_type="uniform")
+    # parameter = DoubleParameter(
+    #     epsilon=0.01, runtimemethod=RunTimeMethod.CRITERION.value, episodes=10000, updatemethod=UpdateMethod.CLIPPED.value, trunc_bounds=TruncatedBounds(lower_bound=10.0, upper_bound=10.0), method=PolicyMethod.EPSILONGREEDY.value)
+    # algo = DoubleStateActionLearning(
+    #     environment=env, policy=agent, algo_params=parameter)
+    # algo.run_double_state_action_learning()
+    # env.reset()
     for _ in range(TOTALSTEPS):
         action = algo.agent.get_action(env.state)
         print(f"action: {action}")
